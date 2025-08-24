dac509d550ff8be8ec68e8e38aca60bfb91c1ba7
kritikaOddeven <kritikaoddeveninfotech@gmail.com>
2025-08-24 21:13:50 +0530

    first_commit

---
+++ resources/views/partials/_sidebar.blade.php
@@ -0,0 +1,138 @@
+<div class="main-sidebar sidebar-style-2">
+                <aside id="sidebar-wrapper">
+                    <div class="sidebar-brand">
+                        <a href="index.html">Society Management</a>
+                    </div>
+                    <div class="sidebar-brand sidebar-brand-sm">
+                        <a href="index.html">SM</a>
+                    </div>
+                    <ul class="sidebar-menu">
+                        <li><a class="nav-link" href="{{url('/')}}"><i class="fas fa-home"></i> <span>Dashboard</span></a></li>
+                        
+                        
+                        <li><a class="nav-link" href="blank.html"><i class="fas fa-users"></i> <span>User</span></a></li>
+
+                        <li><a class="nav-link" href="blank.html"><i class="far fa-user"></i> <span>Owner</span></a></li>
+
+                         <li class="dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i></i> <span>Tenant</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="">Tenant</a></li>
+                                <li><a class="nav-link" href="">Rent</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="fa-regular fa-building"></i></i> <span>Apartment</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="">Tower</a></li>
+                                <li><a class="nav-link" href="">Floor</a></li>
+                                <li><a class="nav-link" href="">Apartment</a></li>
+                                <li><a class="nav-link" href="">Parking</a></li>
+                            </ul>
+                        </li>
+
+                         <li class="dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i> <span>Amenities</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="">Amenities</a></li>
+                                <li><a class="nav-link beep beep-sidebar" href="">Book Amenity</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Bills</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="">Maintenance</a></li>
+                            </ul>
+                        </li>
+
+                        <li><a class="nav-link" href="blank.html"><i class="fas fa-calendar"></i> <span>Events</span></a></li>
+
+                        <li class="dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Report</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="">Maintenance Report</a></li>
+                            </ul>
+                        </li>
+
+                        {{--<li class="dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-map-marker-alt"></i> <span>Google
+                                    Maps</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="gmaps-advanced-route.html">Advanced Route</a></li>
+                                <li><a href="gmaps-draggable-marker.html">Draggable Marker</a></li>
+                                <li><a href="gmaps-geocoding.html">Geocoding</a></li>
+                                <li><a href="gmaps-geolocation.html">Geolocation</a></li>
+                                <li><a href="gmaps-marker.html">Marker</a></li>
+                                <li><a href="gmaps-multiple-marker.html">Multiple Marker</a></li>
+                                <li><a href="gmaps-route.html">Route</a></li>
+                                <li><a href="gmaps-simple.html">Simple</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-plug"></i> <span>Modules</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="modules-calendar.html">Calendar</a></li>
+                                <li><a class="nav-link" href="modules-chartjs.html">ChartJS</a></li>
+                                <li><a class="nav-link" href="modules-datatables.html">DataTables</a></li>
+                                <li><a class="nav-link" href="modules-flag.html">Flag</a></li>
+                                <li><a class="nav-link" href="modules-font-awesome.html">Font Awesome</a></li>
+                                <li><a class="nav-link" href="modules-ion-icons.html">Ion Icons</a></li>
+                                <li><a class="nav-link" href="modules-owl-carousel.html">Owl Carousel</a></li>
+                                <li><a class="nav-link" href="modules-sparkline.html">Sparkline</a></li>
+                                <li><a class="nav-link" href="modules-sweet-alert.html">Sweet Alert</a></li>
+                                <li><a class="nav-link" href="modules-toastr.html">Toastr</a></li>
+                                <li><a class="nav-link" href="modules-vector-map.html">Vector Map</a></li>
+                                <li><a class="nav-link" href="modules-weather-icon.html">Weather Icon</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="far fa-user"></i> <span>Auth</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="auth-forgot-password.html">Forgot Password</a></li>
+                                <li><a href="auth-login.html">Login</a></li>
+                                <li><a href="auth-register.html">Register</a></li>
+                                <li><a href="auth-reset-password.html">Reset Password</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-exclamation"></i> <span>Errors</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="errors-503.html">503</a></li>
+                                <li><a class="nav-link" href="errors-403.html">403</a></li>
+                                <li><a class="nav-link" href="errors-404.html">404</a></li>
+                                <li><a class="nav-link" href="errors-500.html">500</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-bicycle"></i> <span>Features</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="features-activities.html">Activities</a></li>
+                                <li><a class="nav-link" href="features-post-create.html">Post Create</a></li>
+                                <li><a class="nav-link" href="features-posts.html">Posts</a></li>
+                                <li><a class="nav-link" href="features-profile.html">Profile</a></li>
+                                <li><a class="nav-link" href="features-settings.html">Settings</a></li>
+                                <li><a class="nav-link" href="features-setting-detail.html">Setting Detail</a></li>
+                                <li><a class="nav-link" href="features-tickets.html">Tickets</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i> <span>Utilities</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="utilities-contact.html">Contact</a></li>
+                                <li><a class="nav-link" href="utilities-invoice.html">Invoice</a></li>
+                                <li><a href="utilities-subscribe.html">Subscribe</a></li>
+                            </ul>
+                        </li>
+                        <li><a class="nav-link" href="credits.html"><i class="fas fa-pencil-ruler"></i> <span>Credits</span></a>
+                        </li> --}}
+                    </ul>
+
+                    <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+                        <a href="" class="btn btn-primary btn-lg btn-block btn-icon-split">
+                            <i class="fas fa-rocket"></i> Logout
+                        </a>
+                    </div>
+                </aside>
+            </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
 });
