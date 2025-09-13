abb875efa5df9001a3c0ced395e61a93182ebb74
kritikaOddeven <kritikaoddeveninfotech@gmail.com>
2025-09-13 11:01:14 +0530

    merge rent

--- resources/views/apartments/edit.blade.php
+++ resources/views/apartments/edit.blade.php
@@ -113,7 +113,7 @@
                                                 <option value="Unsold" {{ old('status', $apartment->status) == 'Unsold' ? 'selected' : '' }}>Unsold</option>
                                                 <option value="Occupied" {{ old('status', $apartment->status) == 'Occupied' ? 'selected' : '' }}>Occupied</option>
                                                 <option value="Rent" {{ old('status', $apartment->status) == 'Rent' ? 'selected' : '' }}>Avaiable For Rent</option>
-                                                <option value="Rent" {{ old('status', $apartment->status) == 'Rented' ? 'selected' : '' }}>Rented</option>
+                                                <option value="Rent" {{ old('status', $apartment->status) == 'Rented' ? 'selected' : '' }}>On Rent</option>
                                             </select>
                                             @error('status')
                                                 <div class="invalid-feedback">{{ $message }}</div>
